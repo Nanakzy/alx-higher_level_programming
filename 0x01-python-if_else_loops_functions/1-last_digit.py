1dad723c2c645510edea89ff9376fc6df9c5ecab
Nana <nanakzy@gmail.com>
2023-11-28 13:20:05 -0800

    1-last_digit.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -10,5 +10,5 @@ if last_digit > 5:
     print('and is greater than 5')
 if last_digit == 0:
     print('and is 0')
-if last_digit != 0 and last_digit < 6:
+if last_digit != 0 or last_digit < 6:
     print('and is less than 6 and not 0')
