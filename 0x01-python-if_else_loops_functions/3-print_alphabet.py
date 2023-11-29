840bd57f0a34aed6432cf681b9d6714191829f50
Nana <nanakzy@gmail.com>
2023-11-29 03:40:46 -0800

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabet.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python
-for char in range(97, 123):
-    if chr(char) not in ('q', 'e'):
-        print(chr(char), end='')
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,4 @@
-for char in range(ord('a'), ord('z') + 1):
+#!/usr/bin/python3
+for char in range(97, 123):
     if chr(char) not in ('q', 'e'):
         print(chr(char), end='')
