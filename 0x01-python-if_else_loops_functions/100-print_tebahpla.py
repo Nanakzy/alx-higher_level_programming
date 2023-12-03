63c8728c449b9af250c0e517fb1498533c72eeef
Nana <nanakzy@gmail.com>
2023-12-03 09:29:00 -0800

    9-print

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for i in range(ord('z'), ord('A') - 1, -1):
-    print("{:c}".format(i), end="" if i % 2 == 0 or (i >= ord('a') and i <= ord('z')) else "")
-print()
+for i in range(122, 96, -1):
+    if i % 2:
+        i = i - 32
+        print("{:c}".format(i), end=""
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,13 +1,4 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    last_digit = abs(number) % 10
-    result = int(str(number) + str(last_digit))
-    print(result)
-    return last_digit
-
-
-if __name__ == "__main__":
-    print_last_digit(98)
-    print_last_digit(0)
-    r = print_last_digit(-1024)
-    print(r)
+    last_digit = number % 10
+    print(last_digit)
