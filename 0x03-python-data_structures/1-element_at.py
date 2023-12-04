4dbad751aa6df55762ce525af4bab47fa2069a3e
Nana <nanakzy@gmail.com>
2023-12-04 12:38:57 -0800

    1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,18 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0 or idx >= len(my_list):
+    if idx < 0:
+        return None
+    elif idx >= len(my_list):
         return None
     return my_list[idx]
-
-
-# Example usage:
-my_list = [1, 2, 3, 4, 5]
-
-result_valid = element_at(my_list, 2)
-print(result_valid)
-
-result_negative = element_at(my_list, -1)
-print(result_negative)
-
-result_out_of_range = element_at(my_list, 10)
-print(result_out_of_range)
