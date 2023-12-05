694561ec525244493c6c0845e0a7aad374bc8d56
Nana <nanakzy@gmail.com>
2023-12-05 03:21:46 -0800

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
+from itertools import chain
+
+for i in chain(range(122, 96, -1), range(64, 90, -1)):
     if i % 2:
         i = i - 32
-        print("{:c}".format(i), end="")
+    print("{:c}".format(i), end="")
