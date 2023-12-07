10069583738112a0a06ea7a683d78d0fb395f0b0
Nana <nanakzy@gmail.com>
2023-12-07 07:09:17 -0800

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-
+    new_matrix = []
+    for row in matrix:
+        new_row = []
+        for value in row:
+            if isinstance(value, (int, float)):
+                new_row.append(value * value)
+            else:
+                new_row.append(value)
+            new_matrix.append(new_row)
+            return new_matrix
