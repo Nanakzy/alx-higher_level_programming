565bd718ecac5046eb479bdfd44c3fd0cf97d5d8
Nana <nanakzy@gmail.com>
2023-12-19 12:45:43 -0800

    3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Module for Square class."""
+
+
+class Square:
+    """
+    Represents a square.
+
+    Attributes:
+        __size (int): Private instance attribute for the size of the square.
+    """
+    def __init__(self, size=0):
+        """
+        Initializes a new instance of the Square class.
+
+        Args:
+            size (int): The size of the square (default is 0).
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+
+        Returns:
+            None.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """
+        Calculates the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size ** 2
+
