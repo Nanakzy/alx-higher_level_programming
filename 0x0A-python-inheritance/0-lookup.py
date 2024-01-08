7f4fb5e424e2ed18a65401815c0a84b07cda7935
Nana <nanakzy@gmail.com>
2024-01-08 12:05:53 -0800

    0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,12 +1,21 @@
+#!/usr/bin/python3
+
 def lookup(obj):
-    """Returns a list of available attributes and methods of an object."""
-    return [attr for attr in dir(obj) if not callable(getattr(obj, attr)) or hasattr(getattr(obj, attr), '__call__')]
+    """
+    Returns a list of available attributes and methods of an object.
+    """
+    return [attr for attr in dir(obj) if not callable(getattr(obj, attr)) or
+            hasattr(getattr(obj, attr), '__call__')]
 
 # Test cases
+
+
 class MyClass1(object):
     pass
 
+
 class MyClass2(object):
     my_attr1 = 3
+
     def my_meth(self):
         pass
