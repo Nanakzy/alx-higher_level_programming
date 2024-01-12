f366a4e1fe3dc3c1295b654a7f324bc96e395f5f
Nana <nanakzy@gmail.com>
2024-01-12 15:08:08 -0800

    0-lookup

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,19 +1,9 @@
 #!/usr/bin/python3
+"""Defines an object attribute lookup function."""
+
 
 def lookup(obj):
     """
     Returns a list of available attributes and methods of an object.
     """
     return dir(obj)
-
-
-class MyClass1(object):
-    pass
-
-
-class MyClass2(object):
-    my_attr1 = 3
-
-
-def my_mth(self):
-    pass
