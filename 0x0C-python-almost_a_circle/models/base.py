a56ff2691068abfc6df1b80f66287e2743b2d7f8
Nana <nanakzy@gmail.com>
2024-01-19 12:53:28 -0800

    14-main.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Defines a class"""
 
+import json
+
 
 class Base:
     """Base class for managing id attribute."""
@@ -14,3 +16,10 @@ class Base:
         else:
             type(self).__nb_objects += 1
             self.id = type(self).__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return the JSON string representation of list_dictionaries."""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
