1562f57290717dfb4698387027925c8a34cc8505
Nana <nanakzy@gmail.com>
2024-02-13 20:37:21 -0800

    0. List databases

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,7 +1,2 @@
-#!/bin/bash
-
-#MySQL credentials
-MYSQL_USER="username"
-MYSQL_PASSWOED="password"
-
-#mysql -u"$MYSQL_USER" -p"MYSQL_PASSWORD" -e "SHOW DATABASES;"
+-- Lists all databases of my MySQL server.
+SHOW DATABASES;
