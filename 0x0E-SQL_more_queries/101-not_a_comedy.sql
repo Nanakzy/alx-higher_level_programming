fcaf416e73f8ac6f1a0105066f0aaeb4e51f6bb4
Nana <nanakzy@gmail.com>
2024-02-15 12:33:23 -0800

    101-not_a_comedy.sql

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,7 +1,12 @@
--- lists all shows from hbtn_0d_tvshows_rate by their rating
--- lists all rows of a table by the sum of a linked row
-SELECT title, SUM(tv_show_ratings.rate) 'rating'
+-- lists all shows without the genre Comedy in the database hbtn_0d_tvshows
+-- uses a database to list all rows not linked to one row
+SELECT title
 FROM tv_shows
-LEFT JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
+WHERE title NOT IN
+(SELECT title
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = 'Comedy')
 GROUP BY title
-ORDER BY rating ASC;
+ORDER BY title ASC;
