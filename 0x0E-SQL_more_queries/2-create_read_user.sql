397230df603d2b7cd7bc4fe0e9c6da25e9cbec82
Nana <nanakzy@gmail.com>
2024-02-15 03:24:19 -0800

    2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,7 @@
--- Create the database hbtn_0d_2 if it does not already exist
+-- creates the database hbtn_0d_2 and the user user_0d_2
+-- creates a database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
--- Create the user user_0d_2 if it does not already exist and assign a password
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd'
--- should have only SELECT privilege in the database
-GRANT SELECT PRIVILEGE ON hbtn_0d_2.* TO user_0d_2@localhost;
+-- creates a user
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+-- grants SELECT privileges to a user
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
