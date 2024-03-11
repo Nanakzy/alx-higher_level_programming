6eca3f091e643c3158c69275059aca4a236266f4
Nana <nanakzy@gmail.com>
2024-03-11 13:45:35 +0200

    7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -5,8 +5,8 @@ const arg = process.argv[2];
 if (!isNaN(parseInt(arg))) {
   const x = parseInt(arg);
   for (let i = 0; i < x; i++) {
-    console.log("C is fun");
+    console.log('C is fun');
   }
 } else {
-  console.log("Missing number of occurrences");
+  console.log('Missing number of occurrences');
 }
