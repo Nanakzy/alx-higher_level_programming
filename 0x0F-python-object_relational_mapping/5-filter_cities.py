7c04d54890cd812450741321cd59e869cd355a3b
Nana <nanakzy@gmail.com>
2024-03-17 08:27:07 +0200

    5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,30 +1,20 @@
 #!/usr/bin/python3
-"""Lists all cities from the database"""
-
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
-    # Connect to MySQL server
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
-
-    cursor = db.cursor()
-
-    # Use triple quotes for multi-line string
-    query = """SELECT cities.id, cities.name FROM cities
-               JOIN states ON cities.state_id = states.id
-               WHERE states.name = %s ORDER BY cities.id"""
-
-    state_name = sys.argv[4]
-
-    cursor.execute(query, (state_name,))
-
+    cursor= db.cursor()
+    cursor.execute("""SELECT cities.name FROM
+                cities INNER JOIN states ON states.id=cities.state_id
+                WHERE states.name=%s""", (sys.argv[4],))
     cities = cursor.fetchall()
 
     for city in cities:
         print(city)
 
-    # Close cursor and database connection
     cursor.close()
     db.close()
