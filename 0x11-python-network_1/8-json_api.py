ae58a78bda5d134399dd635be905739358f25246
Nana <nanakzy@gmail.com>
2024-03-28 11:15:41 -0700

    8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Script sends a POST request to http://0.0.0.0:5000/search_user with a given letter.
+"""sends POST request to http://0.0.0.0:5000/search_user with a given letter.
 
 Usage: ./8-json_api.py <letter>
   - The letter is sent as the value of the variable `q`.
