a7cff50a883dcc1d3f0ecd961bfd80b0c5e6345e
Nana <nanakzy@gmail.com>
2024-03-28 10:49:21 -0700

    2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Sends a POST request to a given URL with a given email.
+"""Script that sends a POST request to a given URL with a given email.
 
 Usage: ./2-post_email.py <URL> <email>
   - Displays the body of the response.
