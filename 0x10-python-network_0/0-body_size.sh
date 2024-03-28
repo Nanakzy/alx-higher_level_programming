72a0b56b6fff60b57086379af4f5e062eae6ef40
Nana <nanakzy@gmail.com>
2024-03-28 02:15:49 -0700

    task0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # This script takes a URL as input, sends a request to that URL,
 # and displays the size of the body of the response in bytes
-curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
+curl -sI "$1" | grep -i Content-Length | cut -d " " -f 2
