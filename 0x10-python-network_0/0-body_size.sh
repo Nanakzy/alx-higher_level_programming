f92bb3101e8bf0aeaa8cc0d0934e1a3aa3691bde
Nana <nanakzy@gmail.com>
2024-03-28 02:06:44 -0700

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
-
-# Check if exactly one argument (URL) is provided
-if [ "$#" -ne 1 ]; then
-  echo "Usage: $0 URL"
-  exit 1
-fi
-
-# Define the URL from the argument
-url="$1"
-
-# Use curl to get the response and pipe its size (excluding headers) to wc -c
-response_size=$(curl -s -o /dev/null -w "%{size_download}" "$url")
-
-# Display the body size in bytes
-echo "$response_size bytes"
+# This script takes a URL as input, sends a request to that URL,
+# and displays the size of the body of the response in bytes
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
